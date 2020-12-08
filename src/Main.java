11896a69949ae0360fcb322646747645d66e8ad1
paulbatinas <batinaspaul@gmail.com>
2020-12-08 12:09:51 +0200

    tema lab 9

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,20 @@ System.out.println("hello");
 System.out.println("paul");
 byte mybyte1 = 4+6;
 System.out.println(mybyte1);
+byte mybyte2 = 4%3;
+System.out.println(mybyte2);
+byte mybyte3 = 9%4;
+System.out.println(mybyte3);
+byte mybyte4 = -5 + 8 * 6;
+System.out.println(mybyte4);
+byte mybyte5 = (55+9) % 9;
+        System.out.println(mybyte5);
+        byte mybyte6 = 20 + (-3*5) / 8;
+        System.out.println(mybyte6);
+        byte mybyte7 =  5 + (15 / 3 * 2) - (8 % 3);
+        System.out.println(mybyte7);
+
+
 
 
     }
